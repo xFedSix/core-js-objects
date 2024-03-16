ed1ed19e7d4ce85f3743c06320869ceefdc0130b
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-03-16 18:00:51 +0300

    feat: compareObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -82,8 +82,15 @@ function removeProperties(obj, keys) {
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 2}) => true
  *    compareObjects({a: 1, b: 2}, {a: 1, b: 3}) => false
  */
-function compareObjects(/* obj1, obj2 */) {
-  throw new Error('Not implemented');
+function compareObjects(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every((key) => obj1[key] === obj2[key]);
 }
 
 /**

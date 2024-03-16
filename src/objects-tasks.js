23d48bc8efcca201d2311ab21f67332093d80e2d
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-03-16 17:43:16 +0300

    feat: mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,17 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (acc[key]) {
+        acc[key] += value;
+      } else {
+        acc[key] = value;
+      }
+    });
+    return acc;
+  }, {});
 }
 
 /**

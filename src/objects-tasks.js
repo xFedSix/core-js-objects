d0a180e775be3a6a57cc573c1261fa2169764803
Fiodar Siyanka <siyankafiodar@gmail.com>
2024-03-17 20:14:57 +0300

    feat: Rectangle

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -181,8 +181,10 @@ function sellTickets(/* queue */) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = () => this.width * this.height;
 }
 
 /**
